de7c404a7aa51dfa4b57a1b7f443eeab1d789bdb
Marc Vef <vef@uni-mainz.de>
2016-05-04 17:22:39 +0200

    Update: Utilities -- generic shell execute function + print and timestamp

--- src/util/util.py
+++ src/util/util.py
@@ -1,18 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Script contains different useful functions
 # Marc-Andre Vef
 # Version 0.1 (06/19/2015)
 
 import os
-import tarfile
 import sys
 import shutil
-
-
-def timer(start, end):
-    """Returns a string of elapsed time between two timestamps"""
-    hours, rem = divmod(end - start, 3600)
-    minutes, seconds = divmod(rem, 60)
-    return str('{0:0>2}:{1:0>2}:{2:05.2f}'.format(int(hours), int(minutes), seconds))
+import time
+from subprocess import Popen, PIPE
 
 
 def rm_trailing_slash(path):
@@ -29,30 +25,6 @@ def create_dir(path):
         print 'Error: Output directory could not be created.'
         print e.strerror
         sys.exit(1)
-
-
-def file_len(fname):
-    i = 0
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
-
-def cr_compressed_tar(source, compression='bz2'):
-    """creates a tar with compression of gzip or bzip2 (default)
-    @return True if compression sucessful, False if given compression string is invalid"""
-    if compression not in ['bz2', 'gz']:
-        return False
-    # Was done with with clause, but tar file of earlier python versions doesn't support closing with with.
-    try:
-        tar = tarfile.open('%s.tar.%s' % (source, compression), 'w:%s' % compression)
-        tar.add(source, arcname=os.path.basename(source))
-    except tarfile.TarError as e:
-        print e.message
-    finally:
-        tar.close()
-    return True
 
 
 def rm_rf(path):
@@ -73,10 +45,53 @@ def rm_file(path):
         print e.strerror
 
 
-def fprint(input, console=True, filename='', mode='a'):
-    """Function prints input to console by default and also to file if specified"""
-    if console:
-        print input
-    if filename != '':
-        with open(filename, mode) as file:
-            file.write(input)
+def tprint(toprint, nobreak=False):
+    """Function adds current time to input and prints it to the terminal
+
+    Args:
+        toprint (str): The string to be printed.
+        nobreak (bool): True if no line break should occur after print. Defaults to False.
+
+    """
+    curr_time = time.strftime('[%H:%M:%S]')
+    if nobreak:
+        print '%s\t%s' %(curr_time, toprint),
+    else:
+        print '%s\t%s' %(curr_time, toprint)
+
+
+def exec_shell(cmd, suppress_output=False):
+    """Function executes a cmd on the shell
+
+    Args:
+        cmd (str): The command to be executed.
+        suppress_output (bool): Surpresses command output. Defaults to False.
+
+    Returns:
+        (str): Last line of executed command output.
+
+    Raises:
+        OSError: If command could not be executed.
+    """
+    try:
+        if cmd == '':
+            raise OSError("Command string is empty.")
+        # simulate change directory to where mdtest executable is located
+        modified_cmd = '%s' % cmd
+        p = Popen(modified_cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        # Poll process for new output until finished
+        output = ''
+        while True:
+            nextline = p.stdout.readline()
+            if nextline == '' and p.poll() is not None:
+                break
+            output = nextline
+            if not suppress_output:
+                curr_time = time.strftime('[%H:%M:%S]  ')
+                sys.stdout.write('%s%s' % (curr_time, nextline))
+                sys.stdout.flush()
+        stdout, stderr = p.communicate()
+        return output.strip()
+    except OSError as e:
+        print 'ERR when executing shell command'
+        print e.strerror
