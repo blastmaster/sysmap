9df1a25876d04ba6c0e72b46e9f0b961fd05500e
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-06-30 17:44:52 +0200

    system map partitions and mount entries
    begin to implement partition and mount entriy modules to determine which
    partitions are available and what is mounted on a system.

---
+++ sysmap/include/mounts.hpp
@@ -0,0 +1,150 @@
+#ifndef __MOUNTS_HPP__
+#define __MOUNTS_HPP__
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <mntent.h>
+
+
+/**
+ * utility function to split strings by an given delimiter
+ * return the fields in a vector.
+ */
+static
+std::vector<std::string>& split(const std::string& str, const char* delim, std::vector<std::string> &elems)
+{
+    std::stringstream stream(str);
+    std::string item;
+
+    while (getline(stream, item, *delim)) {
+        elems.push_back(item);
+    }
+
+    return elems;
+}
+
+// OVERLOAD
+static
+std::vector<std::string> split(const std::string& str, const char* delim)
+{
+    std::vector<std::string> vec;
+    return split(str, delim, vec);
+}
+
+// OVERLOAD
+static
+std::vector<std::string> split(const char* str, const char* delim)
+{
+    std::string tmp(str);
+    return split(tmp, delim);
+}
+
+
+/**
+ * structure representing a mount entry.
+ * this is one line in mtab/fstab/mountinfo
+ */
+struct mount_entry
+{
+    std::string device_name; // ????
+    std::string mountpoint;
+    std::string fs_type;
+    std::vector<std::string> mount_opts;
+    int dump_freq;
+    int pass_number;
+
+
+    mount_entry(struct mntent mnt) :
+        device_name(mnt.mnt_fsname),
+        mountpoint(mnt.mnt_dir),
+        fs_type(mnt.mnt_type),
+        mount_opts(split(mnt.mnt_opts, ",")),
+        dump_freq(mnt.mnt_freq),
+        pass_number(mnt.mnt_passno) {}
+
+};
+
+// just for debugging
+std::ostream& operator<< (std::ostream& os, const mount_entry& mntent)
+{
+    os << "Name of mounted filesystem: " << mntent.device_name << "\n";
+    os << "Filesytem path prefix: " << mntent.mountpoint << "\n";
+    os << "Mount type: " << mntent.fs_type << "\n";
+    os << "Mount options:\n";
+    for (auto& op : mntent.mount_opts) {
+        os << "\t" << op << "\n";
+    }
+    os << "Dump freq. in days: " << mntent.dump_freq << "\n";
+    os << "Pass number: " << mntent.pass_number << "\n";
+
+    return os;
+}
+
+
+//FIXME
+void load_mounts(const std::string& fname)
+{
+    std::ifstream in(fname);
+
+    if (!in) {
+        std::cerr << "Error cannot open file stream\n";
+        return;
+    }
+
+    for (std::string line; getline(in, line);) {
+        std::vector<std::string> entry;
+        entry = split(line, " ", entry);
+        for (auto &x : entry) {
+            std::cout << "Entry: " << x << "\n";
+        }
+        std::cout << "--------------------\n";
+    }
+
+    in.close();
+
+    return;
+}
+
+
+static
+std::vector<mount_entry> load_mount_with_getmntent(const std::string& fname)
+{
+    FILE *f = setmntent(fname.c_str(), "r");
+    struct mntent* mnt;
+    std::vector<mount_entry> v_me;
+
+    while (mnt = getmntent(f)) {
+        v_me.push_back(mount_entry(*mnt));
+    }
+
+    endmntent(f);
+    return v_me;
+}
+
+
+//int main()
+//{
+    //std::vector<std::string> test_files { "test_data/mounts/prometheusprocmounts.txt",
+                                          //"test_data/mounts/prometheusmtab.txt",
+                                          //"test_data/mounts/prometheusfstab.txt" };
+
+    ////TODO
+    ////load_mounts(filename);
+    //for (auto& fn : test_files) {
+        //auto v = load_mount_with_getmntent(fn);
+        //std::cout << "================================================================================\n";
+        //std::cout << fn << "\n\n";
+        //for (auto& x : v) {
+            //std::cout << x << "\n";
+            //std::cout << "--------------------\n\n";
+        //}
+    //}
+
+    //return 0;
+//}
+
+#endif /* __MOUNTS_HPP__ */
---
+++ sysmap/include/partitions.hpp
@@ -0,0 +1,128 @@
+#ifndef __PARTITIONS_HPP__
+#define __PARTITIONS_HPP__
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <vector>
+
+/**
+ * Get Partitions of a System by parsing /proc/partitions.
+ *
+ * Partition is some basic abstraction type for a partition containing the
+ * values from /proc/partitions and some utility functions for comparing with
+ * other partition objects.
+ */
+
+
+struct Partition {
+    const std::string partition_name;
+    const unsigned int num_blocks;
+    const unsigned int major_dev_num;
+    const unsigned int minor_dev_num;
+
+    Partition(const std::string& name,
+              const unsigned int nb,
+              const unsigned int min,
+              const unsigned int maj) :
+        partition_name(name),
+        num_blocks(nb),
+        major_dev_num(maj),
+        minor_dev_num(min)
+    {}
+
+    bool is_subpartition(const Partition& p) const
+    {
+        return this->major_dev_num == p.major_dev_num;
+    }
+
+    bool operator<(const Partition& p) const
+    {
+        return this->num_blocks < p.num_blocks;
+    }
+
+    bool operator>(const Partition& p) const
+    {
+        return this->num_blocks > p.num_blocks;
+    }
+
+    bool operator==(const Partition& p) const
+    {
+        return (this->major_dev_num == p.major_dev_num && 
+                this->minor_dev_num == p.minor_dev_num &&
+                this->num_blocks == p.num_blocks &&
+                this->partition_name == p.partition_name);
+    }
+
+    bool operator!=(const Partition& p) const
+    {
+        return !(*this == p);
+    }
+};
+
+
+std::ostream& operator<<(std::ostream& os, const Partition& p)
+{
+    os << "Major Device Number: " << p.major_dev_num << "\n";
+    os << "Minor Device Number: " << p.minor_dev_num << "\n";
+    os << "Number of Blocks: " << p.num_blocks << "\n";
+    os << "Partition name: " << p.partition_name << "\n";
+    return os;
+}
+
+/**
+ * load partitions from /proc/partitions file
+ */
+static
+std::vector<Partition> load_partitions(const std::string& fname)
+{
+    std::ifstream in(fname);
+    std::vector<Partition> partitions;
+
+    if (!in) {
+        std::cerr << "No file!\n";
+        return partitions;
+    }
+
+    for (std::string line; getline(in, line);) {
+        std::regex pattern(R"(^\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([^0-9]+.*)$)");
+        std::smatch results;
+        if (std::regex_search(line, results, pattern)) {
+            //std::cout << "Match, " << results.size() << " times!\n";
+            //for (size_t i = 0; i < results.size(); ++i) {
+                //std::cout << i << ": " << results[i] << "\n";
+            //}
+            //FIXME; results is of type std::smatch need some better
+            //initialization than this smatch -> string -> const char* -> int
+            //conversion cascade.
+            Partition tmp(results[4],
+                    atoi(results[3].str().c_str()),
+                    atoi(results[2].str().c_str()),
+                    atoi(results[1].str().c_str()));
+            partitions.push_back(tmp);
+        }
+    }
+
+    in.close();
+    return partitions;
+}
+
+
+/**
+ * just some testing
+ */
+//int main()
+//{
+    //std::vector<std::string> test_files { "taurus_data/loginpartitions.txt",
+                                          //"taurus_data/prometheuspartions.txt",
+                                          //"taurus_data/taurusi6612partitions.txt" };
+    //for (const auto& f : test_files) {
+        //std::cout << f << "\n";
+        //load_partitions(f);
+        //std::cout << "\n\n";
+    //}
+
+    //return 0;
+//}
+
+#endif /* __PARTITIONS_HPP__ */
