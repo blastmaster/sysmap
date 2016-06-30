b0bd4e7d851998d6f20ef8a2563ccaae0430305a
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-06-30 17:48:01 +0200

    simple testing
    Implement some very simple test cases with a few hardcoded assertions, using
    catch as an unit testing framework.

---
+++ sysmap/tests/test_mounts.cpp
@@ -0,0 +1,41 @@
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "mounts.hpp"
+
+
+TEST_CASE("load mounts from proc/mounts", "[mounts]") {
+    std::string filename {"../test_data/mounts/prometheusprocmounts.txt"};
+    auto mvec = load_mount_with_getmntent(filename);
+
+    REQUIRE( mvec.size() == 29 );
+
+    SECTION("test first mount entry in vector") {
+        auto entry = mvec[0];
+
+        REQUIRE(entry.device_name == "sysfs");
+        REQUIRE(entry.mountpoint == "/sys");
+        REQUIRE(entry.fs_type == "sysfs");
+
+        std::vector<std::string> opts {"rw", "nosuid", "nodev", "noexec", "relatime"};
+        REQUIRE(entry.mount_opts == opts);
+
+        REQUIRE(entry.dump_freq == 0);
+        REQUIRE(entry.pass_number == 0);
+    }
+
+    SECTION("test last mount entry in vector") {
+        auto entry = mvec[mvec.size() - 1];
+
+        REQUIRE(entry.device_name == "taurus:/home/soeste/");
+        REQUIRE(entry.mountpoint == "/home/soeste/taurus");
+        REQUIRE(entry.fs_type == "fuse.sshfs");
+
+        std::vector<std::string> mopts {"rw", "nosuid", "nodev", "relatime", "user_id=1000", "group_id=1000"};
+        REQUIRE(entry.mount_opts == mopts);
+        REQUIRE(entry.dump_freq == 0);
+        REQUIRE(entry.pass_number == 0);
+    }
+
+}
