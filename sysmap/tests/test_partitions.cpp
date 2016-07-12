0d6254fbd1dbc1dc8b3513187d9b237079c89b1c
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-07-12 16:55:00 +0200

    adjust proc_partition test

--- sysmap/tests/test_partitions.cpp
+++ sysmap/tests/test_partitions.cpp
@@ -1,19 +1,19 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "partitions.hpp"
+#include "proc_partitions.hpp"
 
 using namespace adafs;
 
 
-TEST_CASE("load partitions from proc/partitions", "[partitions]") {
+TEST_CASE("load partitions from proc/partitions", "[proc_partitions]") {
     std::string filename {"../test_data/partitions/prometheuspartitions.txt"};
-    auto pvec = load_partitions(filename);
+    Proc_Partition::load_partitions(filename);
 
-    REQUIRE(pvec.size() == 6);
+    REQUIRE(Proc_Partition::device_path_cache.size() == 6);
 
     SECTION("test first partition entry (sda)") {
-        auto entry = pvec[0];
+        auto entry = Proc_Partition::proc_partition_cache["/dev/sda"];
 
         REQUIRE(entry.partition_name == "sda");
         REQUIRE(entry.num_blocks == 250059096);
@@ -22,12 +22,18 @@ TEST_CASE("load partitions from proc/partitions", "[partitions]") {
     }
 
     SECTION("test last partition entry (dm-1)") {
-        auto entry = pvec[pvec.size() - 1];
+        auto entry = Proc_Partition::proc_partition_cache["/dev/dm-1"];
 
         REQUIRE(entry.partition_name == "dm-1");
         REQUIRE(entry.num_blocks == 6834176);
         REQUIRE(entry.major_dev_num == 254);
         REQUIRE(entry.minor_dev_num == 1);
+    }
+
+    SECTION("test device name") {
+        auto entry = Proc_Partition::proc_partition_cache["/dev/dm-1"];
+        std::string devname = entry.device_path();
+        REQUIRE(devname == "/dev/dm-1");
     }
 
 }
