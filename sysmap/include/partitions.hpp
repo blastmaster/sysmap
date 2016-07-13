e2e05bb025c44e79a98d5d110602aeb486dec12b
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-07-13 17:15:16 +0200

    using boost/filesystem for partitions
    * Using boost/filesystem makes file handling a lot easier.
      With C++17 this library will go to the STL so its a dependency on time.
    
    * Initialize members of Partition in constructor.
      Add some helper utitlies to source I/O or some string fiddeling out of the
      constructor. Using these helpers in the constructor.

--- sysmap/include/partitions.hpp
+++ sysmap/include/partitions.hpp
@@ -4,10 +4,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <boost/filesystem.hpp>
 
+#include "proc_partitions.hpp"
 #include "mounts.hpp"
 
 namespace adafs {
+
+namespace fs = boost::filesystem;
 
 using Sector = unsigned long long;
 using Byte = unsigned long long;
@@ -20,41 +24,70 @@ static std::string read_sysfs_file(const std::string& name)
     return buf;
 }
 
+static std::string read_sysfs_file(const fs::path& p)
+{
+    return read_sysfs_file(p.string());
+}
+
+static fs::path build_partition_path(const std::string& device_id)
+{
+    fs::path sysdev {"/sys/dev/block"};
+    return sysdev / device_id;
+}
+
+static fs::path build_partition_path(int major_id, int minor_id)
+{
+    std::string s_devid = std::to_string(major_id) + ":" + std::to_string(minor_id);
+    return build_partition_path(s_devid);
+}
+
+static std::string uuid_from_partition(const std::string& device_name)
+{
+    auto d_iter = fs::directory_iterator("/dev/disk/by-uuid/");
+    auto found = std::find_if(fs::begin(d_iter), fs::end(d_iter),
+            [&device_name](const fs::directory_entry& d_ent) {
+                return fs::read_symlink(d_ent).filename().string() == device_name;
+            });
+    if (found == fs::end(d_iter)) {
+        return "";
+    }
+    return (*found).path().filename().string();
+}
+
 class Partition {
 
     public:
-        Partition(const std::string& dev_path) : device_path{dev_path} {}
+        //TODO:
+        //Partition(const std::string& dev_path) : device_path{dev_path} {}
 
-        Partition(const Proc_Partition& pp) : device_path{pp.dev_path()},
-                                              partition_number{pp.minor_dev_num},
+        Partition(const Proc_Partition& pp) : device_path{pp.device_path()},
                                               device_number{pp.major_dev_num},
+                                              partition_number{pp.minor_dev_num},
                                               whole_device{false}
         {
-            //TODO: init sizes
-            static std::string root_sysdev {"/sys/dev/block/"};
-            sector_size = 512; // is this constant? queue/hw_sector_size
-            std::string device_dir = root_sysdev + std::to_string(device_number) +
-                ":" + std::to_string(partition_number) + "/";
-
-            std::string size_file = device_dir + "size";
-            std::string start_file = device_dir + "start";
-
+            //TODO: is this constant? /sys/dev/block/sda/queue/hw_sector_size
+            // need device path _not_ partition path!
+            sector_size = 512;
+            auto device_dir = build_partition_path(device_number, partition_number);
+            fs::path size_file = device_dir / "size";
+            fs::path start_file = device_dir / "start";
             size = stoull(read_sysfs_file(size_file));
             sector_start = stoull(read_sysfs_file(start_file));
-            sector_end = sector_start + size + 1;
+            sector_end = sector_start + size - 1;
+            //TODO: passing just partition name like "sda1" or path "/dev/sda1"
+            //but then as fs::path?
+            uuid = uuid_from_partition(device_path.filename().string());
             //TODO: initialize mountpoints
 
         }
 
-        std::string device_path;
+        fs::path device_path;
         int device_number;
         int partition_number;
         bool whole_device;      // flag indicating if whole device is used by this partition;
         //TODO:
         //Filesystem file_system; // filesytem used on partion
-        //TODO: needs libuuid?
-        //std::string uuid;       // uuid of partition
-        //std::string label;
+        std::string uuid;       // uuid of partition
         Sector size;            // number of sectors
         Sector sector_start;    // sector where partition starts
         Sector sector_end;      // sector where partition ends
