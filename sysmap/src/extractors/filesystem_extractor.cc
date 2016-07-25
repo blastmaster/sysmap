910be74754adf04cee55b2c378a2d8026bf67a7a
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-07-25 16:19:06 +0200

    Basic filesystem extractor
    * This class implements the extractor interface and provides access to the data
      gathered by the concrete extractor.

---
+++ sysmap/src/extractors/filesystem_extractor.cc
@@ -0,0 +1,13 @@
+
+#include "extractors/filesystem_extractor.hpp"
+
+namespace adafs { namespace extractor {
+
+    void Filesystem_Extractor::load()
+    {
+        auto data = collect();
+        // do something useful with data
+
+    }
+
+}} /* closing namespace adafs::extractor */
