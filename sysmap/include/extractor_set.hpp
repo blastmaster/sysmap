13335dc4fe6d2360e635a531d6895b0422df2874
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-07-26 18:09:16 +0200

    Adding Extractor_Set
    * The Extractor_Set type plays a central role in the design. It holds the
      different extractors which should be used to extract the system informations.
      Also it holds the extracted hierachy with the generalized output data.

---
+++ sysmap/include/extractor_set.hpp
@@ -0,0 +1,38 @@
+#ifndef __ADAFS_EXTRACTOR_SET_HPP__
+#define __ADAFS_EXTRACTOR_SET_HPP__
+
+#include "extractor.hpp"
+#include "value.hpp"
+
+#include <memory>
+#include <map>
+
+namespace adafs {
+
+struct Extractor_Set {
+
+    Extractor_Set() {}
+
+    ~Extractor_Set() {}
+
+    // forbid copy construction
+    Extractor_Set(const Extractor_Set&) = delete;
+    // forbid copy assignment
+    Extractor_Set& operator=(const Extractor_Set&) = delete;
+
+    void add_info(const std::string& name, std::unique_ptr<Value> v);
+
+    void add_extractor(const std::string& name, std::shared_ptr<Extractor> const& extr);
+
+    private:
+        // map of extractors to use name -> extractor
+        std::map<std::string, std::shared_ptr<Extractor>> m_extractormap;
+        // map of informations gathered from extractors name -> value
+        std::map<std::string, std::unique_ptr<Value>> m_infomap;
+
+};
+
+
+} /* closing namespace adafs */
+
+#endif /* __ADAFS__EXTRACTOR_SET_HPP__ */
