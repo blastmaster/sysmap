dc9891a22fa8c6fdd1c94acfef5b776cde8aae68
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-08-01 14:59:45 +0200

    Utils get its own cc file
    * Utils is no longer just a single header now its a independent translation unit
      a with a utils.cc file in lib/src.

--- sysmap/lib/include/utils.hpp
+++ sysmap/lib/include/utils.hpp
@@ -12,66 +12,14 @@
 
 namespace adafs { namespace utils {
 
-namespace fs = boost::filesystem;
 
-static
-bool for_each_line(const std::string& path, std::function<bool(const std::string&)> cb)
-{
-    std::ifstream in{path};
-    if (!in) {
-        return false;
-    }
+bool for_each_line(const std::string& path, std::function<bool(const std::string&)> cb);
 
-    for (std::string line; getline(in, line);) {
-        if (! cb(line)) {
-            break;
-        }
-    }
-    return true;
-}
-
-static
-bool for_each_subdirectory(const std::string& path, std::function<bool(const std::string&)> cb)
-{
-    fs::path p{path};
-    auto d_iter = fs::directory_iterator(p);
-    for (const auto& it : d_iter) {
-        if (it.status().type() != fs::directory_file) {
-            continue;
-        }
-        if (! cb(it.path().string())) {
-            break;
-        }
-    }
-    return true;
-}
+void for_each_subdirectory(const std::string& path, std::function<bool(const std::string&)> cb);
 
 std::string read(const std::string& path);
 
 bool read(const std::string& path, std::string& contents);
-
-
-std::string read(const std::string& path)
-{
-    std::string contents;
-    if (! read(path, contents)) {
-        return "";
-    }
-    return contents;
-}
-
-
-bool read(const std::string& path, std::string& contents)
-{
-    std::ifstream in {path};
-    std::ostringstream buffer;
-    if (!in) {
-        return false;
-    }
-    buffer << in.rdbuf();
-    contents = buffer.str();
-    return true;
-}
 
 
 }} /* closing namespace adafs::utils */
---
+++ sysmap/lib/src/utils.cc
@@ -0,0 +1,62 @@
+#include "utils.hpp"
+
+namespace adafs { namespace utils {
+
+namespace fs = boost::filesystem;
+
+
+bool for_each_line(const std::string& path, std::function<bool(const std::string&)> cb)
+{
+    std::ifstream in{path};
+    if (!in) {
+        return false;
+    }
+
+    for (std::string line; getline(in, line);) {
+        if (! cb(line)) {
+            break;
+        }
+    }
+    return true;
+}
+
+
+void for_each_subdirectory(const std::string& path, std::function<bool(const std::string&)> cb)
+{
+    fs::path p{path};
+    auto d_iter = fs::directory_iterator(p);
+    for (const auto& it : d_iter) {
+        if (it.status().type() != fs::directory_file) {
+            continue;
+        }
+        if (! cb(it.path().string())) {
+            break;
+        }
+    }
+}
+
+
+std::string read(const std::string& path)
+{
+    std::string contents;
+    if (! read(path, contents)) {
+        return "";
+    }
+    return contents;
+}
+
+
+bool read(const std::string& path, std::string& contents)
+{
+    std::ifstream in {path};
+    std::ostringstream buffer;
+    if (!in) {
+        return false;
+    }
+    buffer << in.rdbuf();
+    contents = buffer.str();
+    return true;
+}
+
+
+}} /* closing namespace adafs::utils */
