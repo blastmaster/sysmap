81378b8324b2e98783b903b9161b8b77f3f8d706
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-08-04 18:22:42 +0200

    WIP: Output of Map_Values
    * This is _still_ work in progress, and include some dirty hacks.
      We must check if we have an Array_value as second pair of a map entry to
      enable substructuring. There are still some formatting issues.
    
    * Attention: There is a non-member-function is_array_value atm.

--- sysmap/lib/src/map_value.cc
+++ sysmap/lib/src/map_value.cc
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <sstream>
+#include <typeinfo>
+
+#include "array_value.hpp"
 #include "map_value.hpp"
+#include "output.hpp"
 
 namespace adafs {
 
@@ -13,25 +18,34 @@ namespace adafs {
         m_elements.emplace(std::move(name), std::move(value));
     }
 
+    bool is_array_value(const Value* value)
+    {
+        const auto& ti = typeid(*value);
+        return ti == typeid(Array_value);
+    }
+
     std::ostream& Map_value::write(std::ostream& os, const Output_format format, bool quoted) const
     {
         static bool first = true;
         switch (format) {
             case Output_format::XML :
+                XML_Writer xml{os};
+                std::ostringstream tmp(std::ios_base::ate);
                 for (const auto& kvp : m_elements) {
-                    if (first) {
-                        os << "<object ";
-                        first = false;
-                        kvp.second->write(os, format, quoted);
-                        os << "/>\n";
+                    const auto v = kvp.second.get();
+                    if (!first && is_array_value(v)) {
+                        XML_Writer::make_tag(tmp, kvp.first, v);
+                        continue;
                     }
-                    else {
-                        os << " " << kvp.first << "=";
-                        kvp.second->write(os, format, quoted);
-                    }
+                    xml.write_xml_attribute(kvp.first, v);
                 }
-                first = true;
+                first = false;
+                if (!tmp.str().empty()) {
+                    xml.~XML_Writer();
+                    os << tmp.str();
+                }
         }
+
         return os;
     }
 
