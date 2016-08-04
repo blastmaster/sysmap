e3285375cfa466cc0389cdeab40bc4fdb3c93fff
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-08-04 18:20:22 +0200

    Store mountpoints, partitions and disk as array
    * Using Array_value instead of Map_value for mountpoints, partitions and disks.
      The Key for each mountpoint, partition and disk where not used and double
      stored. This is changes makes output simpler.

--- sysmap/lib/src/extractors/disk_extractor.cc
+++ sysmap/lib/src/extractors/disk_extractor.cc
@@ -1,5 +1,6 @@
 #include "value.hpp"
 #include "scalar_value.hpp"
+#include "array_value.hpp"
 #include "map_value.hpp"
 
 #include "extractors/disk_extractor.hpp"
@@ -13,17 +14,17 @@ namespace adafs { namespace extractor {
         auto data = collect();
 
         if (!data.disks.empty()) {
-            auto disks = make_value<Map_value>();
+            auto disks = make_value<Array_value>();
             for (const auto& disk : data.disks) {
                 auto value = make_value<Map_value>();
 
-                value->add("name", make_value<String_value>(disk.name));
+                value->add("name", make_value<String_value>(std::move(disk.name)));
                 value->add("model", make_value<String_value>(std::move(disk.model)));
                 value->add("vendor", make_value<String_value>(std::move(disk.vendor)));
                 value->add("size", make_value<Uint_value>(disk.size));
                 value->add("device_number", make_value<String_value>(std::move(disk.device_number)));
 
-                disks->add(std::move(disk.name), std::move(value));
+                disks->add(std::move(value));
             }
             findings.add_info("disks", std::move(disks));
         }
