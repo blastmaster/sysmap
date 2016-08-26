21169e68d201439db6829d1ce6e5d762773ea9d6
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-08-26 10:18:20 +0200

    testing with main program

--- sysmap/bin/sysmap.cc
+++ sysmap/bin/sysmap.cc
@@ -1,11 +1,25 @@
 #include <iostream>
 
+#include "extractor_set.hpp"
+#include "linux/filesystem_extractor.hpp"
+#include "linux/disk_extractor.hpp"
+#include "hwloc/hwloc_extractor.hpp"
+#include "output.hpp"
 
-//using namespace adafs;
+using namespace adafs;
 
 
 int main(int argc, char** argv)
 {
-    std::cout << "adafs system map\n";
+
+    Extractor_Set extr_set {};
+
+    extr_set.add_extractor("Filesystem", std::make_shared<linux::Filesystem_Extractor>());
+    extr_set.add_extractor("Disks", std::make_shared<linux::Disk_Extractor>());
+    extr_set.add_extractor("hwloc", std::make_shared<hwloc::Hwloc_Extractor>());
+    extr_set.extract();
+    extr_set.write(std::cout, Output_format::XML);
+
+
     return 0;
 }
