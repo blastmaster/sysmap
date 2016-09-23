79cee6dbbbb9bde6ec66cd2224455df2d9f56e54
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-09-23 16:00:59 +0200

    Populate ibnetdiscover output in data structures
    * Refine regular expressions to parse ibnetdiscover output.
    * Assemble output information in appropriate Infiniband::Extractor data
      structures.

--- sysmap/lib/include/linux/infiniband_extractor.hpp
+++ sysmap/lib/include/linux/infiniband_extractor.hpp
@@ -1,6 +1,8 @@
 #ifndef __ADAFS_LINUX_INFINIBAND_EXTRACTOR_HPP__
 #define __ADAFS_LINUX_INFINIBAND_EXTRACTOR_HPP__
 
+#include <regex>
+
 #include "../extractors/infiniband_extractor.hpp"
 
 namespace adafs { namespace linux {
@@ -12,7 +14,9 @@ namespace adafs { namespace linux {
 
         private:
             void collect_ibnetdiscover(data& result);
-
+            std::string assemble_switch(const std::smatch& matches, data& result_graph);
+            std::string assemble_host(const std::smatch& matches, data& result_graph);
+            void assemble_connection(const std::smatch& matches, data& result_graph, const Peer_Type type, const std::string& from_guid);
     };
 
 }} /* closing namespace adafs::linux */
