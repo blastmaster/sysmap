e6c892f0e40bbc77e9ecea0ae12a125215426ef9
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-11-24 23:15:21 +0100

    WIP refactor build system
    * support for git submodules

--- sysmap/cmake/Findsqlite3.cmake
+++ sysmap/cmake/Findsqlite3.cmake
@@ -9,8 +9,8 @@ set(_sqlite3_LIBRARY_SEARCH_DIRS
         "/usr/lib64"
         "/usr/lib/x86_64-linux-gnu")
 
-find_path(SQLITE3_INCLUDE_DIR "sqlite3.h" PATHS ${_sqlite3_HEADER_SEARCH_DIRS})
-find_library(SQLITE3_LIBRARY names "libsqlite3" "sqlite3" HINTS ${_sqlite3_LIBRARY_SEARCH_DIRS})
+find_path(SQLITE3_INCLUDE_DIR NAMES "sqlite3.h" PATHS ${_sqlite3_HEADER_SEARCH_DIRS})
+find_library(SQLITE3_LIBRARY NAMES "libsqlite3" "sqlite3" HINTS ${_sqlite3_LIBRARY_SEARCH_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
