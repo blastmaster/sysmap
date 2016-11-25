dcbff579242ffa50576d3ae2cdd7b06614b2de82
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-11-25 16:49:02 +0100

    add template specializations for json support

---
+++ sysmap/lib/src/scalar_value.cc
@@ -0,0 +1,32 @@
+
+#include "scalar_value.hpp"
+
+using namespace rapidjson;
+
+namespace adafs {
+
+    template<>
+    void String_value::to_json(Writer<OStreamWrapper>& writer) const
+    {
+        writer.String(m_value.c_str());
+    }
+
+    template<>
+    void Uint_value::to_json(Writer<OStreamWrapper>& writer) const
+    {
+        writer.Uint(m_value);
+    }
+
+    template<>
+    void Int_value::to_json(Writer<OStreamWrapper>& writer) const
+    {
+        writer.Int(m_value);
+    }
+
+    template<>
+    void Double_value::to_json(Writer<OStreamWrapper>& writer) const
+    {
+        writer.Double(m_value);
+    }
+
+}
