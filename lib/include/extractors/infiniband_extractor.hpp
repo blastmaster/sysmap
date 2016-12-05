f67ee49ffd8d02d4be5a70543c357e60afe4b381
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-12-05 14:18:24 +0100

    add the from guid to infiniband connections
    * The connections structure in the infiniband extractor has now an
      additional from_guid string attribute. So a connection object stands
      for it self.

--- lib/include/extractors/infiniband_extractor.hpp
+++ lib/include/extractors/infiniband_extractor.hpp
@@ -27,6 +27,7 @@ protected:
         Peer_Type type; // type of peer {switch,host}
         uint64_t local_port; // local port number
         std::string peer_guid; // guid of peer
+        std::string from_guid;
         uint64_t peer_port; // port of peer
         std::string peer_port_guid; // port guid of peer if peer is host
         std::string peer_description; // node description of peer
@@ -60,7 +61,9 @@ protected:
 
 private:
 
-    void load_connections(const std::vector<Connection>& connections, Array_value* value);
+    void load_connections(const std::vector<Connection>& connections,
+                          Array_value* value,
+                          const std::string& from_guid);
 
 };
 
