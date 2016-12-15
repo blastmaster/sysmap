373755b7c1abe66c8cf65fc3b6014bb0fefd1987
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-12-15 21:55:02 +0100

    change infiniband extractor data model
    * Store network connections as separat vector of Connection-Objects.
      Instead of carry them with the host and switch objects.
      This makes access and post processing more easy than before.

--- lib/include/extractors/infiniband_extractor.hpp
+++ lib/include/extractors/infiniband_extractor.hpp
@@ -26,8 +26,8 @@ protected:
     struct Connection {
         Peer_Type type; // type of peer {switch,host}
         uint64_t local_port; // local port number
-        std::string peer_guid; // guid of peer
-        std::string from_guid;
+        std::string peer_guid; // guid of peer or to_guid
+        std::string from_guid; // guid of the other peer ;)
         uint64_t peer_port; // port of peer
         std::string peer_port_guid; // port guid of peer if peer is host
         std::string peer_description; // node description of peer
@@ -42,19 +42,18 @@ protected:
         std::string guid;   // guid
         std::string description; // node description
         std::string port_type;     // type of port 0 {base,enhanced}
-        std::vector<Connection> connections;
     };
 
     struct Host {
         uint64_t num_ports; // number of ports
         std::string guid;   // guid
         std::string description; // node description
-        std::vector<Connection> connections;
     };
 
     struct data {
         std::map<std::string, Host> hosts; // map of hosts with guid as key and host as value
         std::map<std::string, Switch> switches; // map of switches with guid as key and switch as value
+        std::vector<Connection> connections; // vector representing the connections between hosts and switches. 
     };
 
     virtual data collect() = 0;
@@ -62,8 +61,7 @@ protected:
 private:
 
     void load_connections(const std::vector<Connection>& connections,
-                          Array_value* value,
-                          const std::string& from_guid);
+                          Array_value* value);
 
 };
 
