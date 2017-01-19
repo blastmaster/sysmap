e6512a44a0ae83d323f493c3d0261d3e2baeb9d1
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-01-19 15:29:52 +0100

    build: find sqlite3
    * fix sqlite3 find_package.
    * consider environment variables to build proper on systems using
      modules.

--- sysmap/cmake/Findsqlite3.cmake
+++ sysmap/cmake/Findsqlite3.cmake
@@ -9,6 +9,26 @@ set(_sqlite3_LIBRARY_SEARCH_DIRS
         "/usr/lib64"
         "/usr/lib/x86_64-linux-gnu")
 
+# environment variable
+set(_sqlite3_ENV_ROOT "$ENV{SQLITE3_ROOT}")
+
+# if no root is given by user, use environment
+if(NOT SQLITE3_ROOT_DIR AND _sqlite3_ENV_ROOT)
+    set(SQLITE3_ROOT_DIR "${_sqlite3_ENV_ROOT}")
+endif()
+
+if(SQLITE3_ROOT_DIR)
+    set(_sqlite3_HEADER_SEARCH_DIRS
+        "${SQLITE3_ROOT_DIR}"
+        "${SQLITE3_ROOT_DIR}/include"
+        "{_sqlite3_HEADER_SEARCH_DIRS}")
+
+    set(_sqlite3_LIBRARY_SEARCH_DIRS
+        "${SQLITE3_ROOT_DIR}"
+        "${SQLITE3_ROOT_DIR}/lib"
+        "{_sqlite3_LIBRARY_SEARCH_DIRS}")
+endif()
+
 find_path(SQLITE3_INCLUDE_DIR NAMES "sqlite3.h" PATHS ${_sqlite3_HEADER_SEARCH_DIRS})
 find_library(SQLITE3_LIBRARY NAMES "libsqlite3" "sqlite3" HINTS ${_sqlite3_LIBRARY_SEARCH_DIRS})
 
@@ -16,5 +36,5 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
 
 mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY)
-set(SQLITE3_LIBRARIES "${SQLITE_LIBRARY}")
-set(SQLITE3_INCLUDE_DIRS "${SQLITE_INCLUDE_DIR}")
+set(SQLITE3_LIBRARIES "${SQLITE3_LIBRARY}")
+set(SQLITE3_INCLUDE_DIRS "${SQLITE3_INCLUDE_DIR}")
