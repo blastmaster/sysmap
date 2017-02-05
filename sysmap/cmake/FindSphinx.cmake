0c731c5357559b9a9694f9c79fde2472b25302a2
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-02-05 22:26:12 +0100

    WIP building documentation pipline
    * Integrate doxygen for sysmap c++ part API documentataion.
    * Configure doxygen and sphinx config file with the cmake.
      This is work in progress.
      TODO:
      Rework exclude patterns
    * Using breathe sphinx extension as a doxygen-sphinx bridge.
    * Adding first documentation pages to play with.

---
+++ sysmap/cmake/FindSphinx.cmake
@@ -0,0 +1,10 @@
+# try to find sphinx
+
+find_program(SPHINX_EXECUTABLE NAMES sphinx-build
+             HINTS $ENV{SPHINX_DIR} PATH_SUFFIXES bin
+             DOC "sphinx documentation tool")
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
+mark_as_advanced(SPHINX_EXECUTABLE)
