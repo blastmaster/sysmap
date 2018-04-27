8c3aee093b27afe4b1a760329f339af96a037bab
kali pso <kalipso@c3d2.de>
2018-04-27 15:30:05 +0000

    Adding getHostIDs(...) and getEIDs(...) to sysquery.cc Now it is possible to query specific hosts and specific extractors with sysquery --nodes,n (hosts) and --extractors,e Datastructure is finished, next the final query has to be implemented and then parsed into json. after that we could filter even more, to get only specific elements of an extractor

--- bin/sysquery.cc
+++ bin/sysquery.cc
@@ -16,29 +16,74 @@
 using namespace sysmap;
 namespace po = boost::program_options;
 
-std::vector<int> getHostIDs(const std::string dbname, const std::vector<std::string>& names = {}){
+std::map<int, std::vector<int>> getHostIDs(const std::string dbname, const std::vector<std::string>& names = {}){
     sqlite::database db(dbname);
-    utils::log::logging::debug() << 
+    utils::log::logging::debug() <<
         "[sysquery::getHostIDs] getting HostIDs...";
-    std::vector<int> results;
+    std::map<int, std::vector<int>> results;
 
-    auto push_HostID = [&results](int HostID){
-        results.push_back(HostID);
-        std::cout << "Pusing back HostID: " << HostID << std::endl;
+    //returns std::vector<int> of DIDs for the given HostID
+    auto getDIDofHost = [&](int HostID){
+        std::vector<int> DIDs;
+        db << "select DID from Host2Data where HostID = ? ;"
+           << HostID
+           >> [&](int DID) {
+               DIDs.push_back(DID);
+           };
+        return DIDs;
+    };
+
+    //fills resuls with HostID : { DID, DID, DID, ... }
+    auto fillHostID = [&](int HostID){
+        results[HostID] = getDIDofHost(HostID);
+        utils::log::logging::debug() << "[sysquery::getHostIDs]Pusing back HostID: " << HostID;
+        for(auto did : results[HostID]) {
+            utils::log::logging::debug() << "DID: " << did;
+        }
     };
 
     if(names.size() == 0){
         db << "select HostID from Hosttable;"
-             >> push_HostID;
+             >> fillHostID;
         return std::move(results);
     }
 
     for(const auto name : names){
         db << "select HostID from Hosttable where Hostname = ? ;"
            << name
-           >> push_HostID;
+           >> fillHostID;
     }
     return std::move(results);
+}
+
+std::vector<int> getEIDs(const std::string dbname, const std::vector<std::string>& names = {}){
+    std::vector<int> results;
+    sqlite::database db(dbname);
+
+    auto fillEID = [&results](int EID){
+        results.push_back(EID);
+    };
+
+    if(names.size() == 0){
+        db << "select EID from Extractortable;"
+             >> fillEID;
+        return std::move(results);
+    }
+
+    for(const auto name : names){
+        db << "select EID from Extractortable where Name = ? ;"
+           << name
+           >> fillEID;
+    }
+    return std::move(results);
+}
+
+std::stringstream queryToJSON(const std::map<int, std::vector<int>>& hosts, const std::vector<int>& extractors){
+    /* //init jsonstring and fill it with data */
+    /* std::stringstream jsonstring; */
+    /* OStreamWrapper osw(jsonstring); */
+    /* Writer<OStreamWrapper> writer(osw); */
+    /* kv_extr.second->to_json(writer); */
 }
 
 int main(int argc, char** argv)
@@ -51,11 +96,13 @@ int main(int argc, char** argv)
         ("filename,f", po::value<std::string>()->required(), "Filename of the database file")
         ("output,o", po::value<std::string>(), "If specified, output will be written to given file")
         ("nodes,n", po::value<std::vector<std::string>>()->multitoken()->composing(), "Only Query given hosts")
+        ("extractors,e", po::value<std::vector<std::string>>()->multitoken()->composing(), "Only Query given extractors")
         ("query-string,q", po::value<std::string>(), "Query String");
 
     po::variables_map vm;
     std::string filename, query_string;
     std::vector<std::string> hosts {};
+    std::vector<std::string> extractors {};
 
     try
     {
@@ -86,7 +133,14 @@ int main(int argc, char** argv)
         if (vm.count("nodes")){
             hosts = vm["nodes"].as<std::vector<std::string>>(); 
             for (const auto& host : hosts) {
-                utils::log::logging::debug() << "[sysquery] host: " << host << " defined\n";
+                utils::log::logging::debug() << "[sysquery] host: " << host << " defined";
+            }
+        }
+
+        if (vm.count("extractors")){
+            extractors = vm["extractors"].as<std::vector<std::string>>(); 
+            for (const auto& extr : extractors) {
+                utils::log::logging::debug() << "[sysquery] extr: " << extr << " defined";
             }
         }
 
@@ -101,10 +155,9 @@ int main(int argc, char** argv)
     }
 
     utils::log::logging::debug() << "[sysquery] opened database file";
-    /* std::stringstream sstr; */
-    /* rapidjson::OStreamWrapper osw(os); */
 
-    auto hostIDs = getHostIDs(filename, hosts);
+    auto host_map = getHostIDs(filename, hosts);
+    auto extr_vec = getEIDs(filename, extractors);
 
     return 0;
 }
