a49ca292f01e0c6100898f045ebc796629c8a183
kali pso <kalipso@c3d2.de>
2018-09-19 15:56:44 +0000

    Adding utils::parsing to be able to test regexes properly without having to make private memberfunctions public

---
+++ lib/include/utils/parsing.hpp
@@ -0,0 +1,20 @@
+#ifndef __SYSMAP_UTILS_PARSING_HPP__
+#define __SYSMAP_UTILS_PARSING_HPP__
+
+namespace sysmap { namespace utils {
+
+    namespace parsing
+    {
+        /**
+         * Checks if the given string contains an IPv4 Address
+         * @Param ip string of ip
+         * @Return Returns true if an IPv4 addr is found
+         */
+        bool contains_remote_address(const std::string& ip);
+
+        bool is_remote_address(const std::string& ip);
+    }
+
+}} /* closing namespace sysmap::utils */
+
+#endif /* __SYSMAP_UTILS_LOG_HPP__ */
