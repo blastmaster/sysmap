cfa915fb3e6e6bdfb61c38197db2056111a94587
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2020-01-06 09:44:48 +0100

    Add Swap extractor
    Extract information about swap.

---
+++ lib/include/extractors/swap_extractor.hpp
@@ -0,0 +1,38 @@
+#ifndef __SYSMAP_EXTRACTOR_SWAP_EXTRACTOR_HPP__
+#define __SYSMAP_EXTRACTOR_SWAP_EXTRACTOR_HPP__
+
+#include "../extractor_set.hpp"
+#include "../extractor.hpp"
+
+namespace sysmap { namespace extractor {
+
+    struct Swap_Extractor : Extractor {
+
+        Swap_Extractor();
+
+        virtual void load(Extractor_Set& findings) override;
+        virtual void store(Extractor_Set& findings, const std::string& dbname) override;
+
+        protected:
+
+        struct SwapInfo
+        {
+            std::string filename;
+            // type may be an enum (see man /proc/swaps)
+            std::string type;
+            std::uint64_t size;
+            std::uint64_t used;
+        };
+
+        struct data
+        {
+            bool swap_is_active = false;
+            SwapInfo info;
+        };
+
+        virtual data collect() = 0;
+    };
+
+}} /* closing namespace sysmap::extractor */
+
+#endif /* __SYSMAP_EXTRACTOR_SWAP_EXTRACTOR_HPP__ */
